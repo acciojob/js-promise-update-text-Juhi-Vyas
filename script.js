e39c3edf743bf23385d7258e5500a882dc66c356
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-12-28 13:18:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+let otp = document.getElementById("output")
+
+function getResult() {
+	return new Promise((resolve,reject) =>{
+		setTimeout(()=>{
+			resolve()
+		},1000)
+	})
+	.then(res=>{
+		otp.textContent = "Hello, world!"
+	})
+}
+
+getResult()
